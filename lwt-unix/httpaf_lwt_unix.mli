inhabitedtype/httpaf

HTTPS support for the Lwt bindings
based on the comment in #77 regarding TLS integration, this PR adds
HTTPS support to the Lwt bindings.

Async support will be added later - @seliopou, would you like a
different PR?

The approach I took in this PR allows downstream users of http/af to use
either `ocaml-tls` or `lwt_ssl` (build time disambiguation based on
installed libraries - preference is given to `ocaml-tls`). The reason
why I did the extra work of including support for `lwt_ssl` is due to
the fact that `ocaml-tls` doesn't yet include support for elliptic curve
ciphersuites (upstream issue:
https://github.com/mirleft/ocaml-tls/issues/362).

lwt-unix/httpaf_lwt_unix.mli
@@ -46,15 +46,40 @@ module Server : sig
     -> Unix.sockaddr
     -> Lwt_unix.file_descr
     -> unit Lwt.t
+
+  val create_tls_connection_handler
+    :  ?server         : Tls_impl.server
+    -> ?certfile       : string
+    -> ?keyfile        : string
+    -> ?config         : Config.t
+    -> request_handler : (Unix.sockaddr -> Server_connection.request_handler)
+    -> error_handler   : (Unix.sockaddr -> Server_connection.error_handler)
+    -> Unix.sockaddr
+    -> Lwt_unix.file_descr
+    -> unit Lwt.t
 end
 
 (* For an example, see [examples/lwt_get.ml]. *)
 module Client : sig
   val request
-    :  ?config          : Httpaf.Config.t
+    :  ?config          : Config.t
     -> Lwt_unix.file_descr
     -> Request.t
     -> error_handler    : Client_connection.error_handler
     -> response_handler : Client_connection.response_handler
-    -> [`write] Httpaf.Body.t
+    -> [`write] Body.t
+
+  val request_tls
+    :  ?client          : Tls_impl.client
+    -> ?config          : Config.t
+    -> Lwt_unix.file_descr
+    -> Request.t
+    -> error_handler    : Client_connection.error_handler
+    -> response_handler : Client_connection.response_handler
+    -> [`write] Body.t
+end
+
+module Tls_impl : sig
+  type client = Tls_impl.client
+  type server = Tls_impl.server
 end

anmonteiro/httpun

lwt-unix/httpaf_lwt_unix.mli
@@ -47,15 +47,63 @@ module Server : sig
     -> Unix.sockaddr
     -> Lwt_unix.file_descr
     -> unit Lwt.t
+
+  module TLS : sig
+    val create_connection_handler
+      :  ?server         : Tls_io.server
+      -> ?certfile       : string
+      -> ?keyfile        : string
+      -> ?config         : Config.t
+      -> request_handler : (Unix.sockaddr -> Server_connection.request_handler)
+      -> error_handler   : (Unix.sockaddr -> Server_connection.error_handler)
+      -> Unix.sockaddr
+      -> Lwt_unix.file_descr
+      -> unit Lwt.t
+  end
+
+  module SSL : sig
+    val create_connection_handler
+      :  ?server         : Ssl_io.server
+      -> ?certfile       : string
+      -> ?keyfile        : string
+      -> ?config         : Config.t
+      -> request_handler : (Unix.sockaddr -> Server_connection.request_handler)
+      -> error_handler   : (Unix.sockaddr -> Server_connection.error_handler)
+      -> Unix.sockaddr
+      -> Lwt_unix.file_descr
+      -> unit Lwt.t
+  end
 end
 
 (* For an example, see [examples/lwt_get.ml]. *)
 module Client : sig
   val request
-    :  ?config          : Httpaf.Config.t
+    :  ?config          : Config.t
     -> Lwt_unix.file_descr
     -> Request.t
     -> error_handler    : Client_connection.error_handler
     -> response_handler : Client_connection.response_handler
-    -> [`write] Httpaf.Body.t
+    -> [`write] Body.t
+
+  module TLS : sig
+    val request
+      :  ?client          : Tls_io.client
+      -> ?config          : Config.t
+      -> Lwt_unix.file_descr
+      -> Request.t
+      -> error_handler    : Client_connection.error_handler
+      -> response_handler : Client_connection.response_handler
+      -> [`write] Body.t Lwt.t
+  end
+
+  module SSL : sig
+    val request
+      :  ?client          : Ssl_io.client
+      -> ?config          : Config.t
+      -> Lwt_unix.file_descr
+      -> Request.t
+      -> error_handler    : Client_connection.error_handler
+      -> response_handler : Client_connection.response_handler
+      -> [`write] Body.t Lwt.t
+  end
 end