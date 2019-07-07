a7eae05156d69d7e410cde2c7242a5ead09f360a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2019-07-07 15:03:32 -0700

    Fix HTTPs example / bump ppx_tools (#9)

--- examples/lwt/lwt_https_get.ml
+++ examples/lwt/lwt_https_get.ml
@@ -27,3 +27,18 @@ let main port host =
   Body.close_writer request_body;
   finished
 ;;
+
+let () =
+  let host = ref None in
+  let port = ref 443 in
+  Arg.parse
+    ["-p", Set_int port, " Port number (443 by default)"]
+    (fun host_argument -> host := Some host_argument)
+    "lwt_https_get.exe [-p N] HOST";
+  let host =
+    match !host with
+    | None -> failwith "No hostname provided"
+    | Some host -> host
+  in
+  Lwt_main.run (main !port host)
+;;
