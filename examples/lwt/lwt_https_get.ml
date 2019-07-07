6229a06e9da2d24ffaf56c1edf877de720a4ae4c
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2019-07-06 21:56:41 -0700

    HTTPS support for the Lwt bindings (#2)

---
+++ examples/lwt/lwt_https_get.ml
@@ -0,0 +1,44 @@
+open Base
+open Lwt.Infix
+module Arg = Caml.Arg
+
+open Httpaf
+open Httpaf_lwt_unix
+
+let error_handler _ = assert false
+
+let main port host =
+  Lwt_unix.getaddrinfo host (Int.to_string port) [Unix.(AI_FAMILY PF_INET)]
+  >>= fun addresses ->
+  let socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
+  Lwt_unix.connect socket (List.hd_exn addresses).Unix.ai_addr
+  >>= fun () ->
+  let finished, notify_finished = Lwt.wait () in
+  let response_handler =
+    Httpaf_examples.Client.print ~on_eof:(Lwt.wakeup_later notify_finished)
+  in
+  let headers = Headers.of_list [ "host", host ] in
+  Client.TLS.request
+    ~error_handler
+    ~response_handler
+    socket
+    (Request.create ~headers `GET "/")
+  >>= fun request_body ->
+  Body.close_writer request_body;
+  finished
+;;
+
+let () =
+  let host = ref None in
+  let port = ref 443 in
+  Arg.parse
+    ["-p", Set_int port, " Port number (443 by default)"]
+    (fun host_argument -> host := Some host_argument)
+    "lwt_https_get.exe [-p N] HOST";
+  let host =
+    match !host with
+    | None -> failwith "No hostname provided"
+    | Some host -> host
+  in
+  Lwt_main.run (main !port host)
+;;
