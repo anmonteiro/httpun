dcb24260e793b8e98a80936e5cbb5f68a8ed77e8
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-04-05 15:08:20 -0700

    Adapt to TLS v0.11.0 interface (#48)

--- shell.nix
+++ shell.nix
@@ -12,7 +12,10 @@ in
     buildInputs = with ocamlPackages; [ merlin ];
   }).overrideAttrs (o : {
     propagatedBuildInputs = lib.filter
-      (drv: drv.pname == null || !(lib.any (name: name == drv.pname) (lib.attrNames httpafDrvs)))
+    (drv:
+    !(lib.hasAttr "pname" drv) ||
+    drv.pname == null ||
+    !(lib.any (name: name == drv.pname) (lib.attrNames httpafDrvs)))
       o.propagatedBuildInputs;
   })
 
