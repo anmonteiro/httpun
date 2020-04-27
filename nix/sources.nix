95ee1235d3bbdd44bfae68029f10867774bf5d49
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-04-27 16:30:35 -0700

    update sources to make it build
    this is a little circular

--- nix/sources.nix
+++ nix/sources.nix
@@ -2,8 +2,8 @@
 
 let
   overlays = builtins.fetchTarball {
-    url = https://github.com/anmonteiro/nix-overlays/archive/0fdc276.tar.gz;
-    sha256 = "02kxwcgwr5dg0h5m7kiphx2mg5zjphx5va0b7g3igj07l65x9r6i";
+    url = https://github.com/anmonteiro/nix-overlays/archive/ce000d4.tar.gz;
+    sha256 = "0wrqm90cd66rqgpsm1xymjxb5xbjwmaiyw7dx7cx35xadpzil65b";
   };
 
 in
