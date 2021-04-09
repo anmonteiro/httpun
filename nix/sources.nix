41ac60c903329bbf4757225b17dda1984537bae9
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-04-08 23:41:06 -0700

    Fix tests after merge (#90)
    * fix the tests
    
    * update nix sources
    
    * Don't close the request body after the response has completed
    
    The reader might have to ingest more bytes.
    
    Before, we were closing the request body (in `final_write_operation_for`)
    when the writer finished sending the response to signal that we should
    do our "final read operation" .
    
    The request body being closed served 2 meanings:
      1. the current request descriptor can be removed from the request
         queue.
      2. the parser can discard those body bytes (with `Angstrom.advance`).
    
    These 2 are unfortunately at odds with each other when it comes to
    backpressure and error handling:
    
      - in the presence of an error later when reading the request body, we
        need to associate it with the descriptor it came from to find out
        whether we should call the error handler or not (we don't bother
        calling it if we already produced a response).
    
    This only worked until now if there weren't any remaining body bytes to
    be read.

--- nix/sources.nix
+++ nix/sources.nix
@@ -1,18 +1,17 @@
-{ ocamlVersion ? "4_11" }:
+{ ocamlVersion ? "4_12" }:
 
 let
-  overlays =
-    builtins.fetchTarball
-      https://github.com/anmonteiro/nix-overlays/archive/1718c8f.tar.gz;
+  overlays = builtins.fetchTarball
+    https://github.com/anmonteiro/nix-overlays/archive/1e00a821.tar.gz;
 
 in
 
-  import "${overlays}/sources.nix" {
-    overlays = [
-      (import overlays)
-      (self: super: {
-        ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
-            (super.callPackage "${overlays}/ocaml" {});
-      })
-    ];
-  }
+import "${overlays}/sources.nix" {
+  overlays = [
+    (import overlays)
+    (self: super: {
+      ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope'
+        (super.callPackage "${overlays}/ocaml" { });
+    })
+  ];
+}
